1d10e824a4a98c2af4616d08f26e02e4e2a41a23
Marcos <velozmarkdrea@gmail.com>
2016-02-17 17:44:54 +0100

    Making index to instantiate the bootstrap and configurating the routing file

--- public/index.php
+++ public/index.php
@@ -3,3 +3,7 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new \Coolframework\Component\Bootstrap\Bootstrap();
+$app->configRouting(__DIR__ . '/../app/routing/routing.yml');
+
+$controller_index = str_replace("/", "", parse_url($_SERVER['REQUEST_URI'])['path']);
+$app->execute($controller_index);
