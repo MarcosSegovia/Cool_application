40de6c4009ac6f5dc70259d5689a98d78a9aa790
Marcos <velozmarkdrea@gmail.com>
2016-02-22 20:33:00 +0100

    Using Twig Template engine in the CoolController

--- src/CoolController.php
+++ src/CoolController.php
@@ -3,12 +3,20 @@
 namespace CoolApplication;
 
 use Coolframework\Component\Response\Response;
+use Coolframework\Component\Templating\TwigTemplating;
 
 class CoolController
 {
+	private $template_engine;
+
+	public function __construct()
+	{
+		$this->template_engine = new TwigTemplating();
+	}
+
 	public function index()
 	{
-		return new Response('index from CoolController !');
+		return new Response($this->template_engine->render('hello.twig'));
 	}
 
 	public function pam()
