7c8217849434fea1b3a99f0a9c6d6842beac0663
Marcos <velozmarkdrea@gmail.com>
2016-02-24 19:31:49 +0100

    Using the TwigTemplating service provided by the container

--- src/HomeController.php
+++ src/HomeController.php
@@ -2,12 +2,22 @@
 
 namespace CoolApplication;
 
+use Coolframework\Component\Injector\CoolContainer;
 use Coolframework\Component\Response\Response;
 
 class HomeController
 {
+	private $container;
+	private $template_engine;
+
+	public function __construct(CoolContainer $container)
+	{
+		$this->container       = $container;
+		$this->template_engine = $this->container->getService('twig_templating');
+	}
+
 	public function index()
 	{
-		return new Response('First Controller !');
+		return new Response($this->template_engine->render('hello.twig'));
 	}
 }
