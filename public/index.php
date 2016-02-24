f638a46fb5d662fc1368fcef69473a1d648fd7a2
Marcos <velozmarkdrea@gmail.com>
2016-02-24 19:30:47 +0100

    Setting up container and its service definitions in my FC

--- public/index.php
+++ public/index.php
@@ -1,13 +1,17 @@
 <?php
 
+use Symfony\Component\Yaml\Parser;
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
-define('ROOTPATH', __DIR__ . '/../');
+define('ROOTPATH', realpath(__DIR__ . '/../'));
 
-$app = new \Coolframework\Component\Bootstrap\Bootstrap();
-$app->configRouting(__DIR__ . '/../app/routing/routing.yml');
+$parser         = new Parser();
+$cool_container = new \Coolframework\Component\Injector\CoolContainer($parser, ROOTPATH . '/app/services/services.yml');
 
+$app = $cool_container->getService('bootstrap');
+$app->configRouting($parser, __DIR__ . '/../app/routing/routing.yml');
 
-$request = \Coolframework\Component\Request\Request::create();
-$response = $app->execute($request);
+$request  = \Coolframework\Component\Request\Request::create();
+$response = $app->execute($cool_container, $request);
 $response->message();
